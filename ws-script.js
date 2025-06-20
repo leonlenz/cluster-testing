8702601d0f5753a98d83ae8f4f2f7752461788da
LLE <leon.lenz@eleqtron.com>
2025-06-20 08:26:28 +0200

    changed url

--- ws-script.js
+++ ws-script.js
@@ -5,8 +5,8 @@ import { check, sleep } from 'k6';
 import { Trend, Counter } from 'k6/metrics';
 
 // CONFIG
-const API_BASE_URL   = __ENV.API_BASE_URL   || 'https://perfect-starfish.api.whispchat.com';
-const WS_URL         = __ENV.WS_URL         || 'wss://perfect-starfish.api.whispchat.com/api/wsConnect';
+const API_BASE_URL   = __ENV.API_BASE_URL   || 'https://valid-gibbon.api.whispchat.com';
+const WS_URL         = __ENV.WS_URL         || 'wss://valid-gibbon.api.whispchat.com/api/wsConnect';
 const USER_PREFIX    = __ENV.USER_PREFIX    || 'user';
 const PASSWORD       = __ENV.PASSWORD       || 'password123';
 const TOTAL_USERS    = +(__ENV.TOTAL_USERS)    || 1250;
