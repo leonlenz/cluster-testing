da21d2bd93a4822b455ae60996ca5dbb3fccca62
LLE <leon.lenz@eleqtron.com>
2025-07-17 15:38:41 +0200

    updated url

--- ws-script.js
+++ ws-script.js
@@ -5,8 +5,8 @@ import { check, sleep } from 'k6';
 import { Trend, Counter } from 'k6/metrics';
 
 // CONFIG
-const API_BASE_URL   = __ENV.API_BASE_URL   || 'https://faithful-snapper.api.whispchat.com';
-const WS_URL         = __ENV.WS_URL         || 'wss://faithful-snapper.api.whispchat.com/api/wsConnect';
+const API_BASE_URL   = __ENV.API_BASE_URL   || 'https://smiling-sponge.api.whispchat.com';
+const WS_URL         = __ENV.WS_URL         || 'wss://smiling-sponge.api.whispchat.com/api/wsConnect';
 const USER_PREFIX    = __ENV.USER_PREFIX    || 'user';
 const PASSWORD       = __ENV.PASSWORD       || 'password123';
 const TOTAL_USERS    = +(__ENV.TOTAL_USERS)    || 1250;
